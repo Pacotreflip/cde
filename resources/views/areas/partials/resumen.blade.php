34f433f599dbe0da3c51069727872e1e78506fdb
Uziel Bueno <ux.webs@gmail.com>
2015-11-25 10:40:08 -0600

    Se cambia la visualizacion del conteo de areas tipo. - Ahora se visualizan en un panel

--- resources/views/areas/partials/resumen.blade.php
+++ resources/views/areas/partials/resumen.blade.php
@@ -1,10 +1,15 @@
 <hr>
-<h4>Conteo por Áreas Tipo en {{ $area->nombre }}</h4>
 
-<ul class="list-group">
-  @foreach($areas_tipo as $area_tipo)
-  <a class="list-group-item" href="{{ route('tipos.edit', $area_tipo) }}">
-    {{ $area_tipo->ruta(' / ') }} <span class="badge">{{ $area_tipo->areasAsignadasDentroDe($area)->count() }}</span>
-  </a>
-  @endforeach
-</ul>
+<div class="panel panel-default">
+  <div class="panel-heading">
+    Conteo por Áreas Tipo en {{ $area->nombre }}
+  </div>
+
+  <ul class="list-group">
+    @foreach($areas_tipo as $area_tipo)
+    <a class="list-group-item" href="{{ route('tipos.edit', $area_tipo) }}">
+      {{ $area_tipo->ruta(' / ') }} <span class="badge">{{ $area_tipo->areasAsignadasDentroDe($area)->count() }}</span>
+    </a>
+    @endforeach
+  </ul>
+</div>
