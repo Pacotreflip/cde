e42e7cf59b98c58106f052311c581457596ad9c8
EMartinez <EMartinez@PREFEMARTINEZ>
2016-05-26 20:56:59 -0500

    Reporte de desarrollo

--- resources/views/reportes/estado_desarrollo.blade.php
+++ resources/views/reportes/estado_desarrollo.blade.php
@@ -10,12 +10,14 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="list-group">
+                            @if(key_exists("tareas", $lista))
                             @foreach($lista["tareas"] as $tarea)
                             <a href="{{$tarea["atach"]}}" class="list-group-item" target="_blank">
                               <h4 class="list-group-item-heading">{{$tarea["name"]}}</h4>
                               <!--<p class="list-group-item-text">...</p>-->
                             </a>
                             @endforeach
+                            @endif
                           </div>
                     </div>
                 </div>
