aea2c0fa87a0c95f997a626ad518c8675a101add
EMartinez <EMartinez@PREFEMARTINEZ>
2016-07-13 11:43:27 -0500

    Migración en fecha de suministro

---
+++ database/migrations/2016_07_12_114642_create_table_entregas_programadas.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableEntregasProgramadas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Equipamiento.entregas_programadas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_item')->unsigned();
+            $table->float('cantidad_programada');
+            $table->float('cantidad_recibida');
+            $table->date('fecha_entrega');
+            $table->text('observaciones');
+            $table->integer('id_usuario')->nullable();
+            $table->timestamps();
+            
+            $table->foreign('id_item')
+                ->references('id_item')
+                ->on('items')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('Equipamiento.entregas_programadas');
+    }
+}
